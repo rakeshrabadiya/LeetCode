ab13c1858f4891e3244988cbeb55186ea5954912
Rakesh Rabadiya <rakeshrabadiya777@gmail.com>
2026-07-06 09:13:34 +0530

    Time: 0 ms (100%), Space: 42.7 MB (92.61%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26) return false;
+        
+        for(char c = 'a'; c <= 'z'; c++){
+            if(sentence.indexOf(c) < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
