95c5a4d34b0ae301eea4609c9b6fda6d0d3eb528
Rakesh Rabadiya <rakeshrabadiya777@gmail.com>
2026-07-06 09:59:46 +0530

    Time: 1 ms (100%), Space: 44.2 MB (84.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch >= 'A' && ch <= 'Z') {
-                ch = (char)(ch + 32);
-            }
-            if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
-                sb.append(ch);
-            }
-        }
         int i = 0;
-        int j = sb.length() - 1;
+        int j = s.length() - 1;
         while (i < j) {
-            if (sb.charAt(i) != sb.charAt(j)) {
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if (!((ch1 >= 'A' && ch1 <= 'Z') ||
+                  (ch1 >= 'a' && ch1 <= 'z') ||
+                  (ch1 >= '0' && ch1 <= '9'))) {
+                i++;
+                continue;
+            }
+            if (!((ch2 >= 'A' && ch2 <= 'Z') ||
+                  (ch2 >= 'a' && ch2 <= 'z') ||
+                  (ch2 >= '0' && ch2 <= '9'))) {
+                j--;
+                continue;
+            }
+            if (ch1 >= 'A' && ch1 <= 'Z') {
+                ch1 = (char)(ch1 + 32);
+            }
+            if (ch2 >= 'A' && ch2 <= 'Z') {
+                ch2 = (char)(ch2 + 32);
+            }
+            if (ch1 != ch2) {
                 return false;
             }
+
             i++;
             j--;
         }
