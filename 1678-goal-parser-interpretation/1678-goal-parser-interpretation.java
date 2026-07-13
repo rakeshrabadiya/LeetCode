583bd8f43f453a72556716bcf96127c87e8a370d
Rakesh Rabadiya <rakeshrabadiya777@gmail.com>
2026-07-13 11:06:44 +0530

    Time: 1 ms (75.39%), Space: 42.8 MB (68.41%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,6 +1,5 @@
 class Solution {
     public String interpret(String command) {
-        int n = command.length();
         command = command.replace("()","o");
         command = command.replace("(al)","al");
         return command;
